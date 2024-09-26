7bb4a028d8b204c7845bba47610410bf58841a96
Infayou Digital <159997668+anurag8824@users.noreply.github.com>
2024-09-26 13:02:11 +0530

    Update Myproduct.js

--- model/Myproduct.js
+++ model/Myproduct.js
@@ -41,6 +41,10 @@ const MyProducts = new mongoose.Schema({
         type:Boolean,
         default:false,
     },
+    status:{
+   type:String,
+        default:"none"
+    },
 
     
 },{timestamps:true})
